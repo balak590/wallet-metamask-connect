cd9f771d0d420ea58db39d04c7ba561c4fe0be5e
Balak590 <balakumar@britisheducationonline.org>
2021-05-14 14:31:27 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,40 +1,48 @@
 import './App.css';
 import WalletConnectProvider from "@walletconnect/web3-provider";
 import { providers } from "ethers";
-import React, { useEffect } from 'react';
+import React, { useEffect,useState } from 'react';
+var ReactDOM = require('react-dom');
 
 //  Create WalletConnect Provider
 const provider = new WalletConnectProvider({
   rpc: {
-    1: "https://bsc-dataseed.binance.org/"
+    65: "https://bsc-dataseed.binance.org/"
   },
-  qrcode: false,
-  // qrcodeModalOptions: {
-  //   mobileLinks: [
-  //     "metamask",
-  //   ],
-  // },
+  // qrcode: true,
+  qrcodeModalOptions: {
+    mobileLinks: [
+      "metamask",
+    ],
+  },
 });
 
 
 
 function App() {
 
+  const [address,setAddress] = useState("Address");
   useEffect(() => {
     setWallet();
   }, []);
 
   const setWallet = async () =>{
-    await provider.enable();
+    const output = await provider.enable();
+    
 
    const web3Provider = new providers.Web3Provider(provider);
+
+   const signer = web3Provider.getSigner();
+   console.log(signer);
+   setAddress(signer);
+
   }
 
   
   
   return (
     <div className="App">
-    
+    <h1 id="root">address</h1>
       
     </div>
   );
